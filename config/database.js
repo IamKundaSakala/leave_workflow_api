2a14af3b693f325cfde36bb930176340887bf63a
Kunda Sakala <iamkundasakala@gmail.com>
2026-06-27 16:46:32 +0200

    update configs

--- config/database.js
+++ config/database.js
@@ -2,18 +2,30 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: "postgres",
-    password: "DBUser@!!",
-    database: "leave_workflowDB",
-    host: "127.0.0.1",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   test: {
-    username: "postgres",
-    password: "DBUser@!!",
-    database: "leave_workflowDB",
-    host: "127.0.0.1",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
     username: process.env.DB_USERNAME,
